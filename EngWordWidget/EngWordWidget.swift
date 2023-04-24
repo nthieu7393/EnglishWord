7604fd895b14d05138041776cdc3189d70d5b9d1
Hieu Nguyen <nt.hieu7393@gmail.com>
2023-04-24 11:59:56 +0700

    commit

---
+++ EngWordWidget/EngWordWidget.swift
@@ -0,0 +1,81 @@
+import WidgetKit
+import SwiftUI
+
+// MARK: - The Timeline Entry
+struct ViewSizeEntry: TimelineEntry {
+    let date: Date
+    let providerInfo: String
+}
+
+// MARK: - The Widget View
+struct ViewSizeWidgetView : View {
+   
+    let entry: ViewSizeEntry
+
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                
+                // Show view size
+                Text("\(Int(geometry.size.width)) x \(Int(geometry.size.height))")
+                    .font(.system(.title2, weight: .bold))
+                
+                // Show provider info
+                Text(entry.providerInfo)
+                    .font(.footnote)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.green)
+        }
+    }
+}
+
+// MARK: - The Timeline Provider
+struct ViewSizeTimelineProvider: TimelineProvider {
+    
+    typealias Entry = ViewSizeEntry
+    
+    func placeholder(in context: Context) -> Entry {
+        // This data will be masked
+        return ViewSizeEntry(date: Date(), providerInfo: "placeholder")
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (Entry) -> ()) {
+        let entry = ViewSizeEntry(date: Date(), providerInfo: "snapshot")
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        let entry = ViewSizeEntry(date: Date(), providerInfo: "timeline")
+        let timeline = Timeline(entries: [entry], policy: .never)
+        completion(timeline)
+    }
+}
+
+// MARK: - The Widget Configuration
+@main
+struct ViewSizeWidget: Widget {
+    
+    var body: some WidgetConfiguration {
+        StaticConfiguration(
+            kind: "com.hieunguyen.EngWord",
+            provider: ViewSizeTimelineProvider()
+        ) { entry in
+            ViewSizeWidgetView(entry: entry)
+        }
+        .configurationDisplayName("View Size Widget")
+        .description("This is a demo widget.")
+        .supportedFamilies([
+            .systemSmall,
+            .systemMedium,
+            .systemLarge,
+        ])
+    }
+}
+
+struct ViewSizeWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewSizeWidgetView(entry: ViewSizeEntry(date: Date(), providerInfo: "preview"))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
